0ce1926c211e09ba61e28a1d5c9d41f4353edc94
Afnan Mahmood <afnanmahmood52@gmail.com>
2021-06-27 13:15:16 +0500

    1st commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Join from './components/Join/Join'
+import Chat from './components/Chat/Chat'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+        
+            <Route exact path="/">
+              <Join/>
+            </Route>
+
+            <Route path="/chat">
+              <Chat />
+            </Route>
+          
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Join/Join.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Join(props) {
+    return (
+        <div>
+            <h1>Join Component</h1>
+        </div>
+    )
+}
