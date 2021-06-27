128b7aae2d95758d6770d7f0e50f77a076426712
Afnan Mahmood <afnanmahmood52@gmail.com>
2021-06-27 15:04:56 +0500

    frontend work

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {useLocation} from 'react-router-dom'
 
-export default function Chat(props) {
+import querystring from 'query-string'
+import io from 'socket.io-client'
+
+const END_POINT = "http://localhost:5000"
+
+let socket;
+
+export default function Chat() {
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+    const location = useLocation()
+
+    useEffect(()=>{
+        const {name, room} = querystring.parse(location.search)
+
+        socket = io(END_POINT)
+        
+        setName(name)
+        setRoom(room)
+
+        console.log(socket)
+    })
+
     return (
         <div>
             <h1>Chat Component</h1>
--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 const router = require('./router')
-
+const cors = require('cors')
 
 const app = express()
+
 const server = http.createServer(app)
-const io = socketio(server)
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+  });
 
 app.use(router)
+app.use(cors())
 const PORT = process.env.SERVER_PORT_NO
 
 io.on('connection', (socket) => {
